332e50add1dd35988632985ae15b4992161a9b29
Cole Stanfield <cjstanfi@asu.edu>
2020-05-21 15:25:42 -0700

    Initial Commit

---
+++ Bed Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bed Application
+//
+//  Created by Cole Stanfield on 5/21/20.
+//  Copyright © 2020 Cole Stanfield. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Bed ApplicationTests/Bed_ApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bed_ApplicationTests.swift
+//  Bed ApplicationTests
+//
+//  Created by Cole Stanfield on 5/21/20.
+//  Copyright © 2020 Cole Stanfield. All rights reserved.
+//
+
+import XCTest
+@testable import Bed_Application
+
+class Bed_ApplicationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
